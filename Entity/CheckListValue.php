67bfe32b61432ff3d9d66a1f3aad7b6b47358ec5
Rick van Laarhoven <r.vanlaarhoven@opifer.nl>
2014-12-19 09:23:33 +0100

    Added some helper methods to checklistValue

--- Entity/CheckListValue.php
+++ Entity/CheckListValue.php
@@ -11,4 +11,29 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class CheckListValue extends Value
 {
+    /**
+     * Turn value into a simple array
+     *
+     * @return array
+     */
+    public function getOptionsAsArray()
+    {
+        $array = [];
+
+        foreach ($this->options as $option) {
+            $array[] = $option->getId();
+        }
+
+        return $array;
+    }
+
+    /**
+     * Turn the options into a json string
+     *
+     * @return string
+     */
+    public function getOptionsAsJson()
+    {
+        return json_encode($this->getOptionsAsArray());
+    }
 }
