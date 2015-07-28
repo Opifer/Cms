7e167f9c38692e3836185303543196a9eff0e2ba
Dylan Schoenmakers <d.schoenmakers@opifer.nl>
2015-07-28 15:10:57 +0200

    Removed superfluous translator in FormType

--- Form/Type/TemplateType.php
+++ Form/Type/TemplateType.php
@@ -6,15 +6,9 @@ use Opifer\EavBundle\Model\TemplateInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
-use Symfony\Component\Translation\TranslatorInterface;
 
 class TemplateType extends AbstractType
 {
-
-    /**
-     * @var  Symfony\Bundle\FrameworkBundle\Translation\Translator
-     */
-    protected $translator;
 
     /**
      * @var AttributeType
@@ -34,9 +28,8 @@ class TemplateType extends AbstractType
      * @param AttributeType       $attributeType
      * @param string              $templateClass
      */
-    public function __construct(TranslatorInterface $translator, AttributeType $attributeType, $templateClass)
+    public function __construct( AttributeType $attributeType, $templateClass)
     {
-        $this->translator    = $translator;
         $this->attributeType = $attributeType;
         $this->templateClass = $templateClass;
     }
@@ -48,33 +41,33 @@ class TemplateType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('displayName', 'text', [
-            'label' => $this->translator->trans('template.display_name'),
+            'label' => 'template.display_name',
             'attr'  => [
                 'class'                  => 'slugify',
                 'data-slugify-target'    => '.slugify-target',
                 'data-slugify-separator' => '_',
-                'placeholder'            => $this->translator->trans('form.display_name.placeholder'),
-                'help_text'              => $this->translator->trans('form.display_name.help_text')
+                'placeholder'            => 'form.display_name.placeholder',
+                'help_text'              => 'form.display_name.help_text'
             ]
         ])->add('name', 'text', [
-            'label' => $this->translator->trans('template.name'),
+            'label' => 'template.name',
             'attr'  => [
                 'class'       => 'slugify-target',
-                'placeholder' => $this->translator->trans('form.name.placeholder'),
-                'help_text'   => $this->translator->trans('form.name.help_text')
+                'placeholder' => 'form.name.placeholder',
+                'help_text'   => 'form.name.help_text'
             ]
         ])->add('object_class', 'template_object_class', [
-            'label' => $this->translator->trans('template.object_class'),
-            'attr'  => [ 'help_text' => $this->translator->trans('form.object_class.help_text') ]
+            'label' => 'template.object_class',
+            'attr'  => [ 'help_text' => 'form.object_class.help_text']
         ])->add('presentation', 'presentationeditor', [
-            'label' => $this->translator->trans('template.presentation'),
-            'attr'  => [ 'help_text' => $this->translator->trans('form.presentation.help_text') ]
+            'label' => 'template.presentation',
+            'attr'  => [ 'help_text' => 'form.presentation.help_text']
         ])->add('attributes', 'bootstrap_collection', [
             'allow_add'    => true,
             'allow_delete' => true,
             'type'         => $this->attributeType
         ])->add('save', 'submit', [
-            'label' => ucfirst($this->translator->trans('form.submit'))
+            'label' => ucfirst('form.submit')
         ]);
     }
 
