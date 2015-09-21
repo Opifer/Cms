5bee02d26cf92f67c60e6ff3737a0455b8af584e
Rick van Laarhoven <r.vanlaarhoven@opifer.nl>
2015-09-21 16:36:03 +0200

    Small FormBundle fixes

--- Entity/Form.php
+++ Entity/Form.php
@@ -7,7 +7,7 @@ use Gedmo\Mapping\Annotation as Gedmo;
 use Opifer\FormBundle\Model\Form as BaseForm;
 
 /**
- * @ORM\Entity()
+ * @ORM\Entity(repositoryClass="Opifer\FormBundle\Model\FormRepository")
  * @ORM\Table(name="form")
  * @Gedmo\SoftDeleteable(fieldName="deletedAt", timeAware=false)
  */
