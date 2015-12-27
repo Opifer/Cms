9c8c7cf433bc7db7fb029af86af35f58c3e7fe16
Rick van Laarhoven <r.vanlaarhoven@opifer.nl>
2015-12-27 18:28:11 +0100

    Defined some default config

--- DependencyInjection/OpiferCmsExtension.php
+++ DependencyInjection/OpiferCmsExtension.php
@@ -27,6 +27,8 @@ class OpiferCmsExtension extends Extension
             $container->setParameter($key, $value);
         }
 
+        $this->mapClassParameters($config['classes'], $container);
+
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
     }
@@ -40,11 +42,41 @@ class OpiferCmsExtension extends Extension
     public function getParameters($config)
     {
         return [
-            'opifer_cms.autocomplete' => $config['autocomplete'],
-            'opifer_cms.pagination.limit' => $config['pagination']['limit'],
-            'opifer_cms.allowed_locales' => $config['allowed_locales'],
+            'opifer_cms.locale' => $config['locale'],
             'opifer_cms.google_captcha_site_key' => $config['google_captcha_site_key'],
             'opifer_cms.google_captcha_secret' => $config['google_captcha_secret'],
+            'opifer_cms.database_driver' => $config['database']['driver'],
+            'opifer_cms.database_host' => $config['database']['host'],
+            'opifer_cms.database_name' => $config['database']['name'],
+            'opifer_cms.database_user' => $config['database']['user'],
+            'opifer_cms.database_password' => $config['database']['password'],
+            'opifer_cms.database_table_prefix' => $config['database']['table_prefix'],
+            'opifer_cms.autocomplete' => $config['autocomplete'],
+
+            // Deprecated
+            'opifer_cms.allowed_locales' => ['en'],
         ];
+    }
+
+    /**
+     * Remap class parameters.
+     *
+     * @param array $classes
+     * @param ContainerBuilder $container
+     */
+    protected function mapClassParameters(array $classes, ContainerBuilder $container)
+    {
+        foreach ($classes as $model => $serviceClasses) {
+            foreach ($serviceClasses as $service => $class) {
+                $container->setParameter(
+                    sprintf(
+                        'opifer_cms.%s_%s',
+                        $service,
+                        $model
+                    ),
+                    $class
+                );
+            }
+        }
     }
 }
