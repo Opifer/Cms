26774056406a71fe5fb06c6cb2e14903dd8b2fd5
Dylan Schoenmakers <d.schoenmakers@opifer.nl>
2016-01-25 17:15:02 +0100

    Moved block services to Service namespace

--- src/ContentBundle/Block/Service/JumbotronBlockService.php
+++ src/ContentBundle/Block/Service/JumbotronBlockService.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Opifer\ContentBundle\Block;
+namespace Opifer\ContentBundle\Block\Service;
 
 use Opifer\ContentBundle\Block\Tool\ContentTool;
 use Opifer\ContentBundle\Block\Tool\ToolsetMemberInterface;
