904c9b14d83b8a19982839c54932d0e569c93dbd
Rick van Laarhoven <r.vanlaarhoven@opifer.nl>
2016-02-15 10:54:23 +0100

    Small fixes

--- src/ContentBundle/Model/LayoutInterface.php
+++ src/ContentBundle/Model/LayoutInterface.php
@@ -4,5 +4,13 @@ namespace Opifer\ContentBundle\Model;
 
 interface LayoutInterface
 {
+    /**
+     * @return string
+     */
+    public function getFilename();
 
+    /**
+     * @return string
+     */
+    public function getAction();
 }
