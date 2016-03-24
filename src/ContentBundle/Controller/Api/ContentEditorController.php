Opifer/Cms

commit b13bffd22079347ff203d61d99295ece035a4127

Work on shared blocks

src/ContentBundle/Controller/Api/ContentEditorController.php
@@ -73,14 +73,20 @@ class ContentEditorController extends Controller
         $manager  = $this->get('opifer.content.block_manager');
         $response = new JsonResponse;
 
-        $object = $this->get('opifer.content.block_provider_pool')->getProvider($owner)->getBlockOwner($ownerId);
-
         $sort        = $request->request->get('sort');
         $parentId    = $request->request->get('parent');
         $className   = $request->request->get('className');
         $placeholder = (int) $request->request->get('placeholder');
+        $bOwnerId    = (int) $request->request->get('ownerId');
         $data        = $request->request->get('data');
         $data        = json_decode($data, true);
+
+        // In case of editing shared blocks they have no owner
+        if ($bOwnerId === 0) {
+            $object = null;
+        } else {
+            $object = $this->get('opifer.content.block_provider_pool')->getProvider($owner)->getBlockOwner($ownerId);
+        }
 
 //        try {
             $block = $manager->createBlock($object, $className, $parentId, $placeholder, $sort, $data);

src/CmsBundle/Resources/public/js/pagemanager.js
@@ -606,7 +606,7 @@ $(document).ready(function() {
             iFrame.contents().find('body').append(link);
 
             iFrame.contents().find('*[data-pm-block-manage="true"]').each(function() {
-                if ($(this).attr('data-pm-block-owner-id') != ownerId) {
+                if ($(this).attr('data-pm-block-owner-id') != ownerId && $(this).attr('data-pm-block-owner-id') != "0") {
                     $(this).addClass('pm-inherited');
                 }
             });
@@ -761,11 +761,12 @@ $(document).ready(function() {
                     var parent = $(this).closest('*[data-pm-placeholder-key]').closest('*[data-pm-block-id]').attr('data-pm-block-id');
                     var placeholderKey = $(this).closest('*[data-pm-placeholder-key]').attr('data-pm-placeholder-key');
                     var data = $(ui.item).attr('data-pm-block-data');
+                    var blockOwnerId = $(this).closest('*[data-pm-placeholder-key]').closest('*[data-pm-block-id]').attr('data-pm-block-owner-id');
 
                     $(ui.item).attr('data-pm-block-id', '0'); // Set default so toArray won't trip and fall below
                     var sortOrder = $(this).closest('.pm-placeholder').sortable('toArray', {attribute: 'data-pm-block-id'});
 
-                    createBlock({className: className, parent: parent, placeholder: placeholderKey, sort: sortOrder, data: data});
+                    createBlock({className: className, parent: parent, placeholder: placeholderKey, sort: sortOrder, data: data, ownerId: blockOwnerId});
                 }
             };
 
@@ -980,7 +981,6 @@ $(document).ready(function() {
         var doViewSanityChecks = function () {
             paintEmptyPlaceholders();
         };
-
 
         var paintEmptyPlaceholders = function () {
             iFrame.contents().find('.pm-placeholder').each(function (index) {

src/ContentBundle/Block/BlockManager.php
@@ -13,6 +13,7 @@ use Opifer\ContentBundle\Block\Service\BlockServiceInterface;
 use Opifer\ContentBundle\Block\Tool\Toolset;
 use Opifer\ContentBundle\Block\Tool\ToolsetMemberInterface;
 use Opifer\ContentBundle\Entity\Block;
+use Opifer\ContentBundle\Entity\CompositeBlock;
 use Opifer\ContentBundle\Entity\PointerBlock;
 use Opifer\ContentBundle\Model\BlockInterface;
 use Opifer\ContentBundle\Repository\BlockLogEntryRepository;
@@ -449,6 +450,7 @@ class BlockManager
     {
         /** @var AbstractBlockService $service */
         $service = $this->getService($type);
+        $parent = $parentId ? $this->find($parentId, $draft) : null;
 
         // This should replaced with a more hardened function
         if (is_null($data)) {
@@ -457,8 +459,6 @@ class BlockManager
         $data['owner'] = $owner;
 
         $block = $service->createBlock($data);
-
-        $parent = $parentId ? $this->find($parentId, $draft) : null;
 
         $block->setPosition($placeholder);
         $block->setSort(0); // < default, gets recalculated later for entire level
@@ -554,7 +554,8 @@ class BlockManager
      */
     public function makeBlockShared($id)
     {
-        $block = $this->find($id, false);
+        $block = $this->find($id, true);
+        $this->publish($block);
 
         // Duplicate some of the settings to the pointer
         $pointer = new PointerBlock();
@@ -562,14 +563,30 @@ class BlockManager
         $pointer->setParent($block->getParent());
         $pointer->setReference($block);
 
+
         // Detach and make shared
         $block->setShared(true);
+        $block->setSharedName(sprintf('%s_%s', $block->getBlockType(), $block->getOwner()->getId()));
+        $block->setSharedDisplayName(sprintf('%s from %s', $block->getBlockType(), $block->getOwnerName()));
         $block->setParent(null);
         $block->setOwner(null);
         $block->setPosition(null);
         $block->setSort(null);
 
-        $this->save($block)->save($pointer);
+        $this->save($block, false)->save($pointer);
+
+        if ($block instanceof CompositeBlock) {
+            $iterator = new \RecursiveIteratorIterator(
+                new RecursiveBlockIterator($block->getChildren()),
+                \RecursiveIteratorIterator::SELF_FIRST
+            );
+
+            foreach ($iterator as $child) {
+                $child->setOwner(null);
+                $this->publish($child);
+                $this->save($child, false);
+            }
+        }
 
         return $pointer;
     }
@@ -645,7 +662,8 @@ class BlockManager
         // Split arrays into levels by owner (child - parent)
         foreach ($blocks as $block) {
             $pos = array_search($block->getId(), $sort, false);
-            $levels[$block->getOwner()->getId()][$pos] = $block->getId();
+            $ownerId = ($block->getOwner()) ? $block->getOwner()->getId() : 0;
+            $levels[$ownerId][$pos] = $block->getId();
         }
 
         foreach ($levels as &$level) {
@@ -653,11 +671,12 @@ class BlockManager
         }
 
         foreach ($blocks as $block) {
-            $sortedIds = $levels[$block->getOwner()->getId()];
+            $ownerId = ($block->getOwner()) ? $block->getOwner()->getId() : 0;
+            $sortedIds = $levels[$ownerId];
             $pos = array_search(array_search($block->getId(), $sortedIds), array_keys($sortedIds));
             $block->setSort($pos);
 
-            if ($block->getOwner()->getSuper()) {
+            if ($block->getOwner() && $block->getOwner()->getSuper()) {
                 $superId = $block->getOwner()->getSuper()->getId();
                 // See if we need to position it below an inherited block
                 $pos = array_search($block->getId(), $levels[$block->getOwner()->getId()], false);

src/ContentBundle/Block/Service/PointerBlockService.php
@@ -36,56 +36,21 @@ class PointerBlockService extends AbstractBlockService implements BlockServiceIn
 
         $this->blockManager = $blockManager;
     }
-    /**
-     * {@inheritdoc}
-     */
-    public function execute(BlockInterface $block, Response $response = null)
+
+    public function getViewParameters(BlockInterface $block)
     {
-        $this->load($block);
+        $parameters = parent::getViewParameters($block);
 
-        $parameters = array(
-            'block_service'  => $this,
-            'block'          => $block->getReference(),
-        );
-
-        return $this->renderResponse($this->getView($block), $parameters,  $response);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function manage(BlockInterface $block, Response $response = null)
-    {
-        $this->load($block);
-
+        $reference = null;
         if ($block->getReference()) {
-            $reference = $block->getReference();
-//            $reference->setOwner($block->getOwner()); // needed when managing (manage_tags uses this)
-        } else {
-            $reference = $block;
+            $reference = $this->environment->getBlock($block->getReference()->getId());
         }
 
-        $parameters = array(
-            'block_service'  => $this,
-            'pointer'        => $block,
-            'block'          => $reference,
-            'block_view'     => $this->getView($block),
-            'manage_type'    => $this->getManageFormTypeName(),
-        );
+        $custom = [
+            'reference' => $reference,
+        ];
 
-        return $this->renderResponse($this->getManageView($block), $parameters, $response);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getView(BlockInterface $block)
-    {
-        if (!$block->getReference()) {
-            return $this->config['view'];
-        }
-
-        return $this->getReferenceService($block)->getView($block->getReference());
+        return array_merge($parameters, $custom);
     }
 
     public function getReferenceService(BlockInterface $block)
@@ -128,9 +93,10 @@ class PointerBlockService extends AbstractBlockService implements BlockServiceIn
                     'query_builder' => function (EntityRepository $blockRepository) {
                         return $blockRepository->createQueryBuilder('b')
                             ->add('orderBy', 'b.sharedDisplayName ASC')
-                            ->andWhere("b.shared = 1")
+                            ->andWhere("b.shared = :shared")
                             ->andWhere("b.content IS NULL")
-                            ->andWhere("b.template IS NULL");
+                            ->andWhere("b.template IS NULL")
+                            ->setParameter('shared', true);
                         ;
                     },
                 ])

src/ContentBundle/Entity/Block.php
@@ -243,6 +243,18 @@ abstract class Block implements BlockInterface, DraftInterface
         return $this;
     }
 
+    public function getOwnerName()
+    {
+        $owner = $this->getOwner();
+
+        if ($owner === null) {
+            return;
+        } else if ($owner instanceof Content) {
+            return $owner->getTitle();
+        } else if ($owner instanceof Template) {
+            return $owner->getName();
+        }
+    }
 
     /**
      * @return BlockInterface

src/ContentBundle/Environment/Environment.php
@@ -8,19 +8,18 @@
 
 namespace Opifer\ContentBundle\Environment;
 
-use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\PersistentCollection;
 use Opifer\ContentBundle\Block\BlockManager;
 use Opifer\ContentBundle\Block\BlockOwnerInterface;
+use Opifer\ContentBundle\Block\RecursiveBlockIterator;
 use Opifer\ContentBundle\Block\Service\LayoutBlockServiceInterface;
 use Opifer\ContentBundle\Block\Service\PointerBlockService;
+use Opifer\ContentBundle\Entity\PointerBlock;
 use Opifer\ContentBundle\Entity\Template;
 use Opifer\ContentBundle\Model\BlockInterface;
 use Opifer\ContentBundle\Model\Content;
 use Opifer\ContentBundle\Model\TemplatedInterface;
-use Opifer\ContentBundle\Provider\BlockProviderPool;
-use Opifer\Revisions\Exception\DeletedException;
 use Opifer\Revisions\RevisionManager;
 
 class Environment
@@ -195,6 +194,25 @@ class Environment
         $blocks = $this->blockManager->sortBlocks($blocks);
         $cacheKey = $this->getCacheKey();
 
+        // Load shared blocks
+        foreach ($blocks as $block) {
+            $this->blockManager->setDraftVersionFilter(false);
+            if ($block instanceof PointerBlock && $block->getReference()) {
+                $iterator = new \RecursiveIteratorIterator(
+                    new RecursiveBlockIterator(array($block->getReference())),
+                    \RecursiveIteratorIterator::SELF_FIRST
+                );
+
+                foreach ($iterator as $included) {
+                    $reverted = $this->blockManager->revertToDraft($included);
+                    if ($reverted) {
+                        $blocks[] = $reverted;
+                    }
+                }
+            }
+            $this->blockManager->setDraftVersionFilter(true);
+        }
+
         $this->blockCache[$cacheKey] = $blocks;
 
         $this->isLoaded = true;
@@ -204,7 +222,7 @@ class Environment
 
     protected function getBlocksRecursive($owner = null, $blocks = array())
     {
-        $draft = ($this->draft && $owner === $this->object) ? true : false;
+        $draft = ($this->draft && ($owner === $this->object || $owner === null)) ? true : false;
 
         $owned = $this->blockManager->findByOwner($owner, $draft);
 

src/ContentBundle/Model/BlockInterface.php
@@ -16,5 +16,10 @@ interface BlockInterface
      */
     public function getBlockType();
 
+
+    public function isInRoot();
+
+    public function getOwner();
+
     public function setDraft($draft);
 }

src/ContentBundle/Resources/views/Block/Content/pointer.html.twig
@@ -1,1 +1,7 @@
-<div class="well text-muted text-center" {{ manage_tags() }}>shared block</div>
+{% if not reference %}
+    <div class="well text-muted text-center" {{ manage_tags() }}>shared block</div>
+{% else %}
+    <div {{ manage_tags() }}>
+        {{ render_block(reference) }}
+    </div>
+{% endif %}

commit 1f3a0fd4a1f693468a936e7edd204c19ef6aa526

Fixed bug caused in b13bffd22079347ff203d61d99295ece035a4127 that gives error when you try to create blocks in the root

src/ContentBundle/Controller/Api/ContentEditorController.php
@@ -82,7 +82,7 @@ class ContentEditorController extends Controller
         $data        = json_decode($data, true);
 
         // In case of editing shared blocks they have no owner
-        if ($bOwnerId === 0) {
+        if ($bOwnerId === 0 && $parentId !== null) {
             $object = null;
         } else {
             $object = $this->get('opifer.content.block_provider_pool')->getProvider($owner)->getBlockOwner($ownerId);