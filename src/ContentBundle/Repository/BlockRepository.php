c3d3854c0747ff0fcf21ce0052e0421a75b4e1aa
zoran <zoki_m@live.com>
2016-04-01 14:45:53 +0200

    print blocks owner title and link when listing

--- src/ContentBundle/Repository/BlockRepository.php
+++ src/ContentBundle/Repository/BlockRepository.php
@@ -14,7 +14,7 @@ class BlockRepository extends EntityRepository
 	public function getContentByValue($search){
 		
 		return $this->createQueryBuilder('b')
-				->select('c.title, c.slug')
+				->select('b')
 				->innerjoin('b.content', 'c')
 				->where('b.value LIKE :search')
 				->setParameter('search', '%'.$search.'%')
