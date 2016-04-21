9a297bb1fc656f597a9312c59bb7a0057ef96b28
zoran <zoki_m@live.com>
2016-04-21 15:42:13 +0200

    Added priority to aGive priority to title matches in search resultsGive priority to title matches in search results OPICMS-231

--- src/CmsBundle/Repository/ContentRepository.php
+++ src/CmsBundle/Repository/ContentRepository.php
@@ -165,16 +165,42 @@ class ContentRepository extends BaseContentRepository
      */
     public function getRelatedContentToBlocks($search)
     {
-        return $this->createQueryBuilder('c')
-                ->select('c')
-                ->innerjoin('c.blocks', 'b', 'WITH', 'c.id = b.content')
-                ->where('b.value LIKE :search')
-                ->orWhere('c.title LIKE :search')
-                ->orWhere('c.description LIKE :search')
-                ->setParameter('search', '%'.$search.'%')
-                ->groupBy('c.id')
-                ->orderBy('c.id')
-                ->getQuery()
-                ->getResult();
+        $results = $this->createQueryBuilder('c')
+                        ->select('c')
+                        ->innerjoin('c.blocks', 'b', 'WITH', 'c.id = b.content')
+                        ->where('b.value LIKE :search')
+                        ->orWhere('c.title LIKE :search')
+                        ->orWhere('c.description LIKE :search')
+                        ->setParameter('search', '%'.$search.'%')
+                        ->groupBy('c.id')
+                        ->orderBy('c.id')
+                        ->getQuery()
+                        ->getResult();
+
+        return $this->sortSearchResults($results, $search);
+    }
+
+    /**
+     * Sort search results by giving priority to founded by title.
+     *
+     * @param array $results
+     * @param string $search
+     *
+     * @return ArrayCollection
+     */
+    public function sortSearchResults($results, $search)
+    {
+        $sortedResults = array();
+
+        if (!empty($results)) {
+            foreach ($results as $result) {
+                if (stripos($result->getTitle(), $search)) {
+                    array_unshift($sortedResults, $result);
+                } else {
+                    $sortedResults[] = $result;
+                }
+            }
+        }
+        return $sortedResults;
     }
 }
