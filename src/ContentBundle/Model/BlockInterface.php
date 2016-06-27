4cec2d6cb3a35dcaa9d96ec9e7e7a976bec2b019
Rick van Laarhoven <r.vanlaarhoven@opifer.nl>
2016-06-27 16:56:53 +0200

    Fixed editing shared block children from toc

--- src/ContentBundle/Model/BlockInterface.php
+++ src/ContentBundle/Model/BlockInterface.php
@@ -5,28 +5,42 @@ namespace Opifer\ContentBundle\Model;
 interface BlockInterface
 {
     /**
-     * The string returned should match Block's service name
+     * The string returned should match Block's service name.
      *
      * @return string
      */
     public function getBlockType();
 
-
     public function isInRoot();
 
+    /**
+     * @return ContentInterface
+     */
     public function getOwner();
+
+    /**
+     * @param ContentInterface $owner
+     *
+     * @return BlockInterface
+     */
+    public function setOwner(ContentInterface $owner);
+
+    /**
+     * @param BlockInterface $parent
+     */
+    public function setParent($parent);
 
     public function setDraft($draft);
 
     /**
-     * Should return an array of block properties
+     * Should return an array of block properties.
      *
      * @return array
      */
     public function getProperties();
 
     /**
-     * Returns the content
+     * Returns the content.
      *
      * @return ContentInterface
      */
