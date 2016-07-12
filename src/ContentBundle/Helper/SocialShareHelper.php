a9022a06914b0b08e7030d296e95bf69dcba2be5
Rick van Laarhoven <hello@rickvanlaarhoven.nl>
2016-07-12 14:56:19 +0200

    Added a helper to generate social share urls (#205)

---
+++ src/ContentBundle/Helper/SocialShareHelper.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Opifer\ContentBundle\Helper;
+
+class SocialShareHelper
+{
+    /**
+     * @param string $url
+     *
+     * @return string
+     */
+    public function getFacebookShareUrl($url)
+    {
+        return sprintf('http://www.facebook.com/sharer.php?u=%s', urlencode($url));
+    }
+
+    /**
+     * @param string $url
+     *
+     * @return string
+     */
+    public function getTwitterShareUrl($url)
+    {
+        return sprintf('https://twitter.com/intent/tweet?text=%s', urlencode($url));
+    }
+
+    /**
+     * @param string $url
+     *
+     * @return string
+     */
+    public function getGoogleShareUrl($url)
+    {
+        return sprintf('https://plus.google.com/share?url=%s', urlencode($url));
+    }
+
+    /**
+     * @param string $url
+     *
+     * @return string
+     */
+    public function getLinkedInShareUrl($url)
+    {
+        return sprintf('http://www.linkedin.com/shareArticle?mini=true&amp;url=%s', urlencode($url));
+    }
+
+    /**
+     * @param string $url
+     *
+     * @return string
+     */
+    public function getWhatsappShareUrl($url)
+    {
+        return sprintf('whatsapp://send?text=%s', urlencode($url));
+    }
+
+    /**
+     * @param string $url
+     *
+     * @return string
+     */
+    public function getEmailShareUrl($url)
+    {
+        return sprintf('mailto:?subject=&amp;body=%s', urlencode($url));
+    }
+}
