75599209dfc006b9167a7cbd2703d6e5845c60e0
Rick van Laarhoven <hello@rickvanlaarhoven.nl>
2016-11-21 20:15:49 +0100

    Allow re-sending notification emails (#234)

--- src/FormBundle/Controller/PostController.php
+++ src/FormBundle/Controller/PostController.php
@@ -2,6 +2,8 @@
 
 namespace Opifer\FormBundle\Controller;
 
+use Opifer\FormBundle\Mailer\Mailer;
+use Opifer\FormBundle\Model\PostInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -57,6 +59,7 @@ class PostController extends Controller
      */
     public function deleteAction($id)
     {
+        /** @var PostInterface $post */
         $post = $this->get('opifer.form.post_manager')->getRepository()->find($id);
 
         if (!$post) {
@@ -68,6 +71,32 @@ class PostController extends Controller
         $em = $this->getDoctrine()->getManager();
         $em->remove($post);
         $em->flush();
+
+        return $this->redirectToRoute('opifer_form_post_index', ['formId' => $form->getId()]);
+    }
+
+    /**
+     * Re-sends a notification email for the given form post
+     *
+     * @param int $id
+     * @return RedirectResponse|\Symfony\Component\HttpKernel\Exception\NotFoundHttpException
+     */
+    public function notificationAction($id)
+    {
+        /** @var PostInterface $post */
+        $post = $this->get('opifer.form.post_manager')->getRepository()->find($id);
+
+        if (!$post) {
+            return $this->createNotFoundException();
+        }
+
+        $form = $post->getForm();
+
+        /** @var Mailer $mailer */
+        $mailer = $this->get('opifer.form.mailer');
+        $mailer->sendNotificationMail($form, $post);
+
+        $this->addFlash('success', 'Notification mail sent successfully');
 
         return $this->redirectToRoute('opifer_form_post_index', ['formId' => $form->getId()]);
     }
