dd924ff5074b46b09cc65bdab29ec9b503363c41
Rick van Laarhoven <r.vanlaarhoven@opifer.nl>
2016-12-07 09:45:55 +0100

    Disabled unavailable shared block edit and delete actions

--- src/CmsBundle/Controller/Backend/BlockController.php
+++ src/CmsBundle/Controller/Backend/BlockController.php
@@ -22,35 +22,30 @@ class BlockController extends BaseBlockController
         $tableAlias = $source->getTableAlias();
 
         $source->manipulateQuery(
-            function ($query) use ($tableAlias)
-            {
-                $query->andWhere("{$tableAlias}.shared = 1")
-                        ->andWhere("{$tableAlias}.content IS NULL")
-                        ->andWhere("{$tableAlias}.template IS NULL");
+            function ($query) use ($tableAlias) {
+                $query
+                    ->andWhere("{$tableAlias}.shared = 1")
+                    ->andWhere("{$tableAlias}.content IS NULL")
+                    ->andWhere("{$tableAlias}.template IS NULL")
+                ;
             }
         );
-//
-//        $idColumn = new NumberColumn(array('id' => 'id', 'field' => 'id', 'title' => 'label.id'));
-//        $nameColumn = new TextColumn(array('id' => 'name', 'field' => 'sharedName', 'title' => 'label.name'));
-//        $displayNameColumn = new TextColumn(array('id' => 'display_name', 'field' => 'sharedDisplayName', 'title' => 'label.display_name'));
 
-        $editAction = new RowAction('button.edit', 'opifer_content_block_shared_edit');
-        $editAction->setRouteParameters(['id']);
+        // TODO: Implement an edit action
+        //$editAction = new RowAction('button.edit', 'opifer_content_block_shared_edit');
+        //$editAction->setRouteParameters(['id']);
 
-        $deleteAction = new RowAction('button.delete', 'opifer_content_block_delete');
-        $deleteAction->setRouteParameters(['id']);
+        //$deleteAction = new RowAction('button.delete', 'opifer_content_block_delete');
+        //$deleteAction->setRouteParameters(['id']);
 
         /* @var $grid \APY\DataGridBundle\Grid\Grid */
         $grid = $this->get('grid');
         $grid->setId('blocks')
             ->setSource($source)
-//            ->setColumns(new Columns($this->get('security.context')))
-//            ->addColumn($idColumn)
-//            ->addColumn($nameColumn)
-//            ->addColumn($displayNameColumn)
             ->setVisibleColumns(['id', 'sharedName', 'sharedDisplayName'])
-            ->addRowAction($editAction)
-            ->addRowAction($deleteAction);
+            //->addRowAction($editAction)
+            //->addRowAction($deleteAction)
+        ;
 
         return $grid->getGridResponse('OpiferCmsBundle:Backend/Block:shared.html.twig');
     }
