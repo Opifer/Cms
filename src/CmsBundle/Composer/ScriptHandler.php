8f8e77234d0e87e102b7f32db99dd98f718b8df4
Rick van Laarhoven <hello@rickvanlaarhoven.nl>
2017-02-22 11:44:02 +0100

    Install npm packages with unsafe permissions

--- src/CmsBundle/Composer/ScriptHandler.php
+++ src/CmsBundle/Composer/ScriptHandler.php
@@ -22,7 +22,7 @@ class ScriptHandler
             throw new \RuntimeException("Running bower install failed with $return_var\n");
         }
 
-        exec('npm install', $output, $return_var);
+        exec('npm install --unsafe-perm', $output, $return_var);
         $event->getIO()->write('<info>'.implode("\n", $output).'</info>');
         if ($return_var) {
             throw new \RuntimeException("Running npm failed with $return_var\n");
