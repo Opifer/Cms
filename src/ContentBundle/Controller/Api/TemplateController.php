420d1ff412d80fafd2a2707d3654b43ad7193074
Rick van Laarhoven <hello@rickvanlaarhoven.nl>
2017-04-07 14:18:46 +0200

    Added template API endpoint

---
+++ src/ContentBundle/Controller/Api/TemplateController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Opifer\ContentBundle\Controller\Api;
+
+use JMS\Serializer\SerializationContext;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Opifer\ContentBundle\Environment\Environment;
+use Opifer\ContentBundle\Model\ContentInterface;
+use Opifer\ContentBundle\Provider\TemplateBlockProvider;
+use Opifer\ContentBundle\Serializer\BlockExclusionStrategy;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class TemplateController extends Controller
+{
+    /**
+     * @ApiDoc()
+     *
+     * @param int $id
+     *
+     * @return ContentInterface[]
+     */
+    public function getTemplateAction(Request $request, $id)
+    {
+        $version = $request->query->get('_version');
+
+        /** @var TemplateBlockProvider $templateBlockProvider */
+        $templateBlockProvider = $this->get('opifer.content.template_block_provider');
+        $template = $templateBlockProvider->getBlockOwner($id);
+
+        /** @var Environment $environment */
+        $environment = $this->get('opifer.content.block_environment');
+        $environment->setObject($template);
+
+        if (null !== $version && $this->isGranted('ROLE_ADMIN')) {
+            $environment->setDraft(true);
+        }
+
+        $environment->load();
+
+        $context = SerializationContext::create();
+
+        $blocks = $environment->getBlocks();
+        $context->setGroups(['Default', 'detail'])
+            ->enableMaxDepthChecks();
+
+        $contentItem = [
+            'id' => $template->getId(),
+            'name' => $template->getName(),
+            'display_name' => $template->getDisplayName(),
+            'blocks' => $blocks,
+        ];
+
+        $json = $this->get('jms_serializer')->serialize($contentItem, 'json', $context);
+
+        return new JsonResponse(json_decode($json, true));
+    }
+}
