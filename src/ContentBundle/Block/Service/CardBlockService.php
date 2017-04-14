6ccebe7ef8d4d48d69a30d8bea90c00ee4be8ece
Stefan van den Broek <stefan@soweare.nl>
2017-04-14 15:33:43 +0200

    Remove fields

--- src/ContentBundle/Block/Service/CardBlockService.php
+++ src/ContentBundle/Block/Service/CardBlockService.php
@@ -8,12 +8,10 @@ use Opifer\ContentBundle\Block\Tool\ToolsetMemberInterface;
 use Opifer\ContentBundle\Entity\CardBlock;
 use Opifer\ContentBundle\Form\Type\ContentPickerType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Opifer\MediaBundle\Form\Type\MediaPickerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Opifer\ContentBundle\Model\BlockInterface;
-use Symfony\Component\Validator\Constraints\NotBlank;
 
 /**
  * Card Block Service
@@ -26,7 +24,6 @@ class CardBlockService extends AbstractBlockService implements BlockServiceInter
     public function buildManageForm(FormBuilderInterface $builder, array $options)
     {
         parent::buildManageForm($builder, $options);
-
         $builder->get('default')
             ->add('header', CKEditorType::class, [
                 'label' => 'label.header',
@@ -48,46 +45,11 @@ class CardBlockService extends AbstractBlockService implements BlockServiceInter
             ])
         ;
         $builder->get('properties')
-            ->add('displaySize', ChoiceType::class, [
-                'label' => 'label.list_display_size',
-                'choices'  => [
-                    null => 'Default',
-                    'sm' => 'Small',
-                    'md' => 'Medium',
-                    'lg' => 'Large',
-                ],
-                'required' => true,
-                'constraints' => [
-                    new NotBlank(),
-                ],
-                'expanded' => true,
-                'multiple' => false,
-                'attr' => [
-                    'help_text' => 'help.list_display_size', 
-                    'class' => 'btn-group btn-group-styling', 
-                    'data-toggle' => 'buttons',
-                    'tag' => 'styles'
-                ],
-                'label_attr' => ['class' => 'btn'],
-            ])
             ->add('preset', ChoiceType::class, [
                 'label'       => 'Preset',
-                'attr'        => [
-                    'help_text' => 'Pick a preset',
-                    'tag' => 'styles'
-                ],
+                'attr'        => ['help_text' => 'Pick a preset', 'tag' => 'styles'],
                 'choices'     => $this->config['presets'],
                 'required'    => true,
-                'constraints' => [
-                    new NotBlank(),
-                ],
-            ])
-
-            ->add('background', ChoiceType::class, [
-                'required' => false,
-                'label' => 'label.background_color',
-                'choices' => $this->config['backgrounds'],
-                'attr'  => ['tag' => 'styles']
             ])
             ->add('styles', ChoiceType::class, [
                 'label' => 'label.styling',
@@ -96,26 +58,15 @@ class CardBlockService extends AbstractBlockService implements BlockServiceInter
                 'expanded' => true,
                 'multiple' => true,
                 'attr' => [
-                    'help_text' => 'help.html_styles', 
+                    'help_text' => 'help.html_styles',
                     'class' => 'radio-rows',
                     'tag' => 'styles'
                 ],
             ])
         ;
-
         $builder->get('properties')
-            ->add('id', TextType::class, [
-                'attr' => [
-                    'help_text' => 'help.html_id'
-                ],
-                'required' => false
-            ])
-            ->add('extra_classes', TextType::class, [
-                'attr' => [
-                    'help_text' => 'help.extra_classes'
-                ],
-                'required' => false,
-            ])
+            ->add('id', TextType::class, ['attr' => ['help_text' => 'help.html_id']])
+            ->add('extra_classes', TextType::class, ['attr' => ['help_text' => 'help.extra_classes']])
             ->add('content',  ContentPickerType::class, [
                 'as_object' => false,
                 'label' => 'label.content',
