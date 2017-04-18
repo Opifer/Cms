711818e7ec4418bf57197c1f074a1fe2e54fd74b
Stefan van den Broek <stefan@soweare.nl>
2017-04-18 15:56:52 +0200

    Remove replaceLinks from ContentExtension

--- src/ContentBundle/Twig/ContentExtension.php
+++ src/ContentBundle/Twig/ContentExtension.php
@@ -207,48 +207,7 @@ class ContentExtension extends \Twig_Extension
 
         return $string;
     }
-
-    /**
-     * Replaces all links that matches the pattern with content urls.
-     *
-     * @param string $string
-     *
-     * @return string
-     */
-    protected function replaceLinks($string)
-    {
-        preg_match_all('/(\[content_url\](.*?)\[\/content_url\]|\[content_url\](.*?)\[\\\\\/content_url\])/', $string, $matches);
-
-        if (!count($matches)) {
-            return $string;
-        }
-
-        if(!empty($matches[3])){
-            $matches[1] = $matches[3];
-        }
-
-        /** @var Content[] $contents */
-        $contents = $this->getContentManager()->getRepository()->findByIds($matches[1]);
-        $array = [];
-        foreach ($contents as $content) {
-            $array[$content->getId()] = $content;
-        }
-
-        foreach ($matches[0] as $key => $match) {
-            if (isset($array[$matches[1][$key]])) {
-                $content = $array[$matches[1][$key]];
-
-                $url = $this->getRouter()->generate('_content', ['slug' => $content->getSlug()]);
-            } else {
-                $url = $this->getRouter()->generate('_content', ['slug' => '404']);
-            }
-
-            $string = str_replace($match, $url, $string);
-        }
-
-        return $string;
-    }
-
+    
     /**
      * @param string|ContentInterface $content
      * @param ContentInterface        $child
