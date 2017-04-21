80af27862a18f698fd5e60c006f57a38b3241bb8
Stefan van den Broek <stefan@soweare.nl>
2017-04-21 16:34:20 +0200

    no message

--- src/CmsBundle/Entity/Domain.php
+++ src/CmsBundle/Entity/Domain.php
@@ -13,6 +13,7 @@ class Domain
 
     /**
      * @var string
+     * 
      * @Assert\Regex(
      *     pattern="/^(?:[-A-Za-z0-9]+\.)+[A-Za-z]{2,6}$/",
      *     message="The domain should be a valid domain"
