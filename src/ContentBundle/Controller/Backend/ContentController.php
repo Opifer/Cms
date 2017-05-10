8f92b2ee2cafc20c9b07936514695bda963bc564
Stefan van den Broek <stefan@soweare.nl>
2017-05-10 14:51:46 +0200

    If there is no site skip site selection

--- src/ContentBundle/Controller/Backend/ContentController.php
+++ src/ContentBundle/Controller/Backend/ContentController.php
@@ -72,7 +72,12 @@ class ContentController extends Controller
     {
         $sites = $this->getDoctrine()->getRepository(Site::class)->findAll();
 
-        //if one site go on
+        //if no site go on
+        if (count($sites) == 0) {
+            return $this->redirectToRoute('opifer_content_content_select_type');
+        }
+
+        //if just one site select this one
         if (count($sites) == 1) {
             return $this->redirectToRoute('opifer_content_content_select_type', ['siteId' => $sites[0]->getId()]);
         }
