d74f89df863d9aff48b031948f19296eb0c33d7e
Stefan van den Broek <stefan@soweare.nl>
2017-05-17 16:24:02 +0200

    Created ability to add site to domain (#327)

--- src/CmsBundle/Form/Type/DomainType.php
+++ src/CmsBundle/Form/Type/DomainType.php
@@ -3,6 +3,8 @@
 namespace Opifer\CmsBundle\Form\Type;
 
 use Opifer\CmsBundle\Entity\Domain;
+use Opifer\CmsBundle\Entity\Site;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -24,6 +26,16 @@ class DomainType extends AbstractType
                 ],
                 'required' => true
             ])
+
+            ->add('site', EntityType::class, [
+                'label' => 'label.site',
+                'class'    => Site::class,
+                'property' => 'name',
+                'attr'     => [
+                    'help_text'   => 'help_text.site',
+                ],
+                'required' => true
+            ]);
         ;
     }
 
