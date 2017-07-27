f215e9a6fe59b1e5500edaa72a3694b8f6cf1dc2
Stefan van den Broek <stefan@soweare.nl>
2017-07-27 11:13:49 +0200

    Made the code clearer

--- src/CmsBundle/Controller/Frontend/ExceptionController.php
+++ src/CmsBundle/Controller/Frontend/ExceptionController.php
@@ -4,35 +4,30 @@ namespace Opifer\CmsBundle\Controller\Frontend;
 
 use Opifer\CmsBundle\Entity\Locale;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class ExceptionController extends Controller
 {
     /**
      * 404 error page.
-     *
+     * 
+     * @param Request $request
      * @return Response
      */
-    public function error404Action()
+    public function error404Action(Request $request)
     {
 
-        $slugParts = explode('/', $this->getRequest()->getPathInfo());
-        $localeRepository= $this->getDoctrine()->getRepository(Locale::class);
-        $locale = $localeRepository->findOneByLocale($slugParts[1]);
+        $slugParts = explode('/', $request->getPathInfo());
+        $locale = $this->getDoctrine()->getRepository(Locale::class)
+            ->findOneByLocale($slugParts[1]);
 
         $contentRepository = $this->getDoctrine()->getRepository('OpiferCmsBundle:Content');
 
-        if($locale) {
-            $content = $contentRepository->findOneBySlug($locale->getLocale().'/404');
-            if ($content) {
-                return $this->forward('OpiferContentBundle:Frontend/Content:view', [
-                    'content' => $content,
-                    'statusCode' => 404,
-                ]);
-            }
+        if (!$locale || !$content = $contentRepository->findOneBySlug($locale->getLocale().'/404')) {
+            $content = $contentRepository->findOneBySlug('404');
         }
 
-        $content = $contentRepository->findOneBySlug('404');
         if ($content) {
             return $this->forward('OpiferContentBundle:Frontend/Content:view', [
                 'content' => $content,
