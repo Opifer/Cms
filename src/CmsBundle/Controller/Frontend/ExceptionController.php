cee7143d3216640019aaaa85169de8744b01f707
Stefan van den Broek <stefan@soweare.nl>
2017-07-27 11:53:37 +0200

    Render 404 page from correct locale (#359)
    * Created a check to see if there is a 404 available for the locale of the unfindable page
    
    * Made the code clearer

--- src/CmsBundle/Controller/Frontend/ExceptionController.php
+++ src/CmsBundle/Controller/Frontend/ExceptionController.php
@@ -2,20 +2,32 @@
 
 namespace Opifer\CmsBundle\Controller\Frontend;
 
+use Opifer\CmsBundle\Entity\Locale;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class ExceptionController extends Controller
 {
     /**
      * 404 error page.
-     *
+     * 
+     * @param Request $request
      * @return Response
      */
-    public function error404Action()
+    public function error404Action(Request $request)
     {
+
+        $slugParts = explode('/', $request->getPathInfo());
+        $locale = $this->getDoctrine()->getRepository(Locale::class)
+            ->findOneByLocale($slugParts[1]);
+
         $contentRepository = $this->getDoctrine()->getRepository('OpiferCmsBundle:Content');
-        $content = $contentRepository->findOneBySlug('404');
+
+        if (!$locale || !$content = $contentRepository->findOneBySlug($locale->getLocale().'/404')) {
+            $content = $contentRepository->findOneBySlug('404');
+        }
+
         if ($content) {
             return $this->forward('OpiferContentBundle:Frontend/Content:view', [
                 'content' => $content,
